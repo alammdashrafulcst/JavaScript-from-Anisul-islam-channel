f779ae1cfacd14feb8cdea749d6dceb626558bc3
ashraful <alammdashrafulcst@gmail>
2021-06-23 23:52:02 +0600

    9 and 10 tutorial code are here

---
+++ tutorial10/index10.js
@@ -0,0 +1,24 @@
+var num1 = prompt("Enter first number:");
+
+var num2 = prompt("Enter second number:");
+
+
+num1 = parseInt(num1,10);
+
+num2 = parseInt(num2,10);
+
+var sum ,sub;
+
+sum = num1 +num2 ;
+
+sub = num1 - num2 ;
+
+
+document.write(" sum is " + sum + "<br/>");
+
+document.write(" sub is " + sub);
+
+
+
+
+
